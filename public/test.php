73d19f7430da1271787cf6337220a0893045f7fc
cleer73 <jake@cleer.net>
2013-03-28 21:06:13 -0700

    Added comments to the main execution path. Refactored the success response body to include the number of records being return. Updated the default reps count to be random between 500 and 100 records, for stressing the database as well as the webserver.

--- public/test.php
+++ public/test.php
@@ -1,20 +1,24 @@
 <?php
 
-$reps = rand(1, 100);
+$reps = rand(500, 1000);
 $results = array();
 
 
 try {
+    // Connection
     $db = new PDO('mysql:host=localhost;dbname=benchmarks', 'root');
 
+    // Build a query statement
     $query = $db->prepare('SELECT * FROM users WHERE id = :id');
 
+    // Get a bunch of crap.
     for ($i=0; $i < $reps; $i++) {
         $query->execute(array(':id' => rand(1, 400000)));
         $results[] = $query->fetch(PDO::FETCH_ASSOC);
     }
 
-    json_response($results);
+    // Respond.
+    json_response(array('count'=>count($results), 'data'=>$results));
 } catch (PDOException $e) {
     json_response(array('message'=>'DATABASE FAILURE: '.$e->getMessage()));
 }
@@ -22,5 +26,5 @@ try {
 
 function json_response($data) {
     header('Content-Type: application/json');
-    print(json_encode($data, JSON_NUMERIC_CHECK));
+    exit(json_encode($data, JSON_NUMERIC_CHECK));
 }
